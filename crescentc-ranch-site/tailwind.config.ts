a476ebe995b5203d2e88f5afdd196c230e65fb2e
wcbiedenharn-dev <wcbiedenharn@gmail.com>
2025-08-14 21:30:47 -0500

    Delete crescentc-ranch-site directory

--- crescentc-ranch-site/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-
-import type { Config } from "tailwindcss";
-const config: Config = {
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
-export default config;
